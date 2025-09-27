77582889b93bb0b948f67f5119f72851a394f641
idalgo-2021 <mr.samuelson@ya.ru>
2025-09-27 16:47:10 +0600

    добавил таблицу Вх.сообщений с бронями род ТГ

--- backend/migrations/20250924233233_baseline.sql
+++ backend/migrations/20250924233233_baseline.sql
@@ -8,26 +8,33 @@ CREATE TABLE "public"."listing_types" (
   PRIMARY KEY ("id"),
   CONSTRAINT "listing_types_slug_key" UNIQUE ("slug")
 );
+
+
+
+
 -- Create "listings" table
 CREATE TABLE "public"."listings" (
   "id" uuid NOT NULL DEFAULT gen_random_uuid(),
-  "title" text NOT NULL,
-  "description" text NULL,
-  "listing_type_id" integer NOT NULL,
+  "code" uuid NOT NULL, -- идентификатор в системе OTA
+  "title" text NOT NULL, -- наименование
+  "description" text NULL, -- описание
+  "main_picture" text NULL, -- картинка объекта
+  "listing_type_id" integer NOT NULL, -- код типа объекта
   "address" text NULL,
   "city" text NULL,
   "country" text NULL,
   "latitude" double precision NULL,
   "longitude" double precision NULL,
   "created_at" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  "is_active" boolean NOT NULL DEFAULT true,
-  "code" uuid NOT NULL,
-  "main_picture" text NULL,
   PRIMARY KEY ("id"),
   CONSTRAINT "listings_listing_type_id_fkey" FOREIGN KEY ("listing_type_id") REFERENCES "public"."listing_types" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
 -- Create index "listings_code_uniq_key" to table: "listings"
 CREATE UNIQUE INDEX "listings_code_uniq_key" ON "public"."listings" ("code");
+
+
+
 -- Create "roles" table
 CREATE TABLE "public"."roles" (
   "id" serial NOT NULL,
@@ -57,26 +64,71 @@ CREATE TABLE "public"."assignment_statuses" (
   PRIMARY KEY ("id"),
   CONSTRAINT "assignment_statuses_slug_key" UNIQUE ("slug")
 );
+
+
+
 -- Create "assignments" table
 CREATE TABLE "public"."assignments" (
   "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "ota_sg_reservation_id" uuid NULL, -- привязка к бронированию
+  -- "code" uuid NOT NULL, -- идентификатор в системе OTA
+
   "listing_id" uuid NOT NULL,
   "purpose" text NOT NULL,
+
   "created_at" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  "expires_at" timestamp NOT NULL,
-  "reporter_id" uuid NOT NULL,
-  "accepted_at" timestamp NULL,
-  "deadline" timestamp NULL,
+  "expires_at" timestamp NOT NULL, -- после наступления этой даты, предложение протухает(становится неактивным)
+  
+  "reporter_id" uuid NOT NULL, -- привязанный ТГ
+  "accepted_at" timestamp NULL, -- дата принятия предложения со стороны ТГ
+  "declined_at" timestamp NULL, -- дата отклонения предложения со стороны ТГ
+
+  "deadline" timestamp NULL, -- Дедлайн(дата) СДАЧИ отчета (устанавливается после accepted_at)
+
   "status_id" integer NOT NULL DEFAULT 1,
-  "code" uuid NOT NULL,
-  "declined_at" timestamp NULL,
+  
   PRIMARY KEY ("id"),
+  CONSTRAINT "assignments_ota_sg_reservation_id_key" UNIQUE ("ota_sg_reservation_id"),
   CONSTRAINT "assignments_listing_id_fkey" FOREIGN KEY ("listing_id") REFERENCES "public"."listings" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT "assignments_reporter_id_fkey" FOREIGN KEY ("reporter_id") REFERENCES "public"."users" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT "assignments_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "public"."assignment_statuses" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
 );
--- Create index "assignments_code_uniq_key" to table: "assignments"
-CREATE UNIQUE INDEX "assignments_code_uniq_key" ON "public"."assignments" ("code");
+
+
+-- Create "ota_sg_reservation_statuses" table
+CREATE TABLE "public"."ota_sg_reservation_statuses" (
+  "id" serial NOT NULL,
+  "slug" text NOT NULL,
+  "name" text NOT NULL,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "ota_sg_reservation_statuses_slug_key" UNIQUE ("slug")
+);
+-- Create "ota_sg_reservations" table - Бронирования Островка под тайных гостей
+CREATE TABLE "public"."ota_sg_reservations" (
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "created_at" timestamp NULL DEFAULT CURRENT_TIMESTAMP,  -- дата создания записи в ota_sg_reservations
+  "source_msg" jsonb NULL,   -- исходное сообщение из OTA(JSON-портянка, с указанием объекта, его типа и параметров брони)
+  
+  "ota_id" uuid NOT NULL,    -- идентификатор брони в системе OTA(читаем из sourse_msg)
+  "booking_number" text NOT NULL, -- код бронирования(ТГ показывает вахтёру при заселении)(читаем из sourse_msg)
+  
+  "listing_id" uuid NOT NULL,     -- к какому объекту относится(читаем из sourse_msg)
+  "checkin_date" timestamp NOT NULL,  -- дата начала брони, когда надо въехать(читаем из sourse_msg)
+  "checkout_date" timestamp NOT NULL, -- окончание брони, когда надо выезжать(читаем из sourse_msg)
+  "status_id" integer NOT NULL,
+ 
+  PRIMARY KEY ("id"),
+  CONSTRAINT "ota_sg_reservations_ota_id_key" UNIQUE ("ota_id"),
+  -- CONSTRAINT "ota_sg_reservations_booking_number_key" UNIQUE ("booking_number"),
+  CONSTRAINT "ota_sg_reservations_listing_id_fkey" FOREIGN KEY ("listing_id") REFERENCES "public"."listings" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "ota_sg_reservations_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "public"."ota_sg_reservation_statuses" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+
+
+
+
+
 -- Create "answer_types" table
 CREATE TABLE "public"."answer_types" (
   "id" serial NOT NULL,
